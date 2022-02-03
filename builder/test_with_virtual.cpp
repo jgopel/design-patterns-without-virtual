88904aabc07d9ccbfa7758efe788681dd3ef8733
Jonathan Gopel <jgopel@quantlab.com>
2022-02-03 07:29:22 -0700

    :sparkles: [builder] Implement Builders
    Problem:
    - The director needs multiple builders to demonstrate its capabilities.
    - There are no builders.
    
    Solution:
    - Implement a builder with dependent ordering and one with independent
      ordering.

--- builder/test_with_virtual.cpp
+++ builder/test_with_virtual.cpp
@@ -15,3 +15,83 @@ TEST(ProductTest, ConstructProductWithSomeArgs) {
   EXPECT_EQ(product.size, std::nullopt);
   EXPECT_STREQ(product.model.value().c_str(), "Excalibur");
 }
+
+TEST(BuilderTest, CanBuildAProductWithAllArgs) {
+  IndependentBuilder builder{};
+  builder.set_color();
+  builder.set_size();
+  builder.set_model();
+  const auto product = builder.build();
+  EXPECT_EQ(product.color.value(), Color::Green);
+  EXPECT_EQ(product.size.value(), 42);
+  EXPECT_STREQ(product.model.value().c_str(), "Independent");
+}
+
+TEST(BuilderTest, CanBuildAProductWithSomeArgs) {
+  IndependentBuilder builder{};
+  builder.set_color();
+  const auto product = builder.build();
+  EXPECT_EQ(product.color.value(), Color::Green);
+  EXPECT_FALSE(product.size.has_value());
+  EXPECT_FALSE(product.model.has_value());
+}
+
+TEST(BuilderTest, CanBuildAProductWithNoArgs) {
+  const IndependentBuilder builder{};
+  const auto product = builder.build();
+  EXPECT_FALSE(product.color.has_value());
+  EXPECT_FALSE(product.size.has_value());
+  EXPECT_FALSE(product.model.has_value());
+}
+
+TEST(BuilderTest, IndependentBuilderIsABuilder) {
+  static_assert(std::is_base_of_v<BuilderBase, IndependentBuilder>);
+}
+
+TEST(BuilderTest, IndependentBuilderCanConstructInArbitraryOrder) {
+  {
+    IndependentBuilder builder{};
+    builder.set_color();
+    const auto product = builder.build();
+    EXPECT_EQ(product.color.value(), Color::Green);
+    EXPECT_FALSE(product.size.has_value());
+    EXPECT_FALSE(product.model.has_value());
+  }
+
+  {
+    IndependentBuilder builder{};
+    builder.set_size();
+    const auto product = builder.build();
+    EXPECT_FALSE(product.color.has_value());
+    EXPECT_EQ(product.size.value(), 42);
+    EXPECT_FALSE(product.model.has_value());
+  }
+
+  {
+    IndependentBuilder builder{};
+    builder.set_model();
+    const auto product = builder.build();
+    EXPECT_FALSE(product.color.has_value());
+    EXPECT_FALSE(product.size.has_value());
+    EXPECT_STREQ(product.model.value().c_str(), "Independent");
+  }
+}
+
+TEST(BuilderTest, DependentBuilderIsABuilder) {
+  static_assert(std::is_base_of_v<BuilderBase, DependentBuilder>);
+}
+
+TEST(BuilderTest, DependentBuilderHasModelWithSize) {
+  DependentBuilder builder{};
+  builder.set_size();
+  builder.set_model();
+  const auto product = builder.build();
+  EXPECT_FALSE(product.color.has_value());
+  EXPECT_EQ(product.size.value(), 101);
+  EXPECT_STREQ(product.model.value().c_str(), "Dependent - size 101");
+}
+
+TEST(BuilderTest, DependentBuilderHasNoModelWithoutSize) {
+  DependentBuilder builder{};
+  EXPECT_THROW((void)builder.set_model(), std::bad_optional_access);
+}
--- builder/with_virtual.hpp
+++ builder/with_virtual.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "utilities/rule_of_five.hpp"
+
 #include <optional>
 #include <string>
 
@@ -14,4 +16,48 @@ public:
   std::optional<Color> color{};
   std::optional<int> size{};
   std::optional<std::string> model{};
+};
+
+class BuilderBase {
+public:
+  BuilderBase() = default;
+  RULE_OF_FIVE_VIRTUAL_DESTRUCTOR_FOR_BASE(BuilderBase);
+
+  constexpr virtual auto set_color() -> void = 0;
+  constexpr virtual auto set_size() -> void = 0;
+  constexpr virtual auto set_model() -> void = 0;
+
+  [[nodiscard]] virtual auto build() const -> Product = 0;
+};
+
+class IndependentBuilder : public BuilderBase {
+public:
+  IndependentBuilder() = default;
+  RULE_OF_FIVE_VIRTUAL_DESTRUCTOR_FOR_OVERRIDE(IndependentBuilder);
+
+  constexpr auto set_color() -> void override { product.color = Color::Green; }
+  constexpr auto set_size() -> void override { product.size = 42; }
+  constexpr auto set_model() -> void override { product.model = "Independent"; }
+
+  [[nodiscard]] auto build() const -> Product override { return product; }
+
+private:
+  Product product{};
+};
+
+class DependentBuilder : public BuilderBase {
+public:
+  DependentBuilder() = default;
+  RULE_OF_FIVE_VIRTUAL_DESTRUCTOR_FOR_OVERRIDE(DependentBuilder);
+
+  constexpr auto set_color() -> void override { product.color = Color::Blue; }
+  constexpr auto set_size() -> void override { product.size = 101; }
+  constexpr auto set_model() -> void override {
+    product.model = "Dependent - size " + std::to_string(product.size.value());
+  }
+
+  [[nodiscard]] auto build() const -> Product override { return product; }
+
+private:
+  Product product{};
 };
